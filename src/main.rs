d29c575926ef330919647347e53d5d529b2cd77d
Ryan Porterfield <ryan@ryanporterfield.com>
2022-03-28 23:31:00 -0600

    RSR-8 Logging configuration
    - Add log4rs configuration
    - Add /log/ to .gitignore

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,16 @@
 #![warn(missing_docs)]
 #![deny(rustdoc::missing_crate_level_docs)]
 
+use log::info;
+
 mod repository;
 mod types;
 
 #[tokio::main]
 async fn main() -> Result<(), ()> {
-    println!("Hello world!");
+    log4rs::init_file("log4rs.yaml", Default::default()).unwrap();
+
+    info!("Hello world!");
 
     Ok(())
 }
